809e95d435aa3e9e9afb9ec7c4146686cc288d92
Al Riefqy <ekydesmito@gmail.com>
2024-03-19 14:06:46 +0800

    update export

--- resources/views/page/project/export_project.blade.php
+++ resources/views/page/project/export_project.blade.php
@@ -23,6 +23,12 @@
             <th rowspan="3">Level Assessment Document Status</th>
             <th rowspan="3">Business Case Document Status</th>
             <th rowspan="3">Remark</th>
+            <th rowspan="3">Assessment Form Status</th>
+            <th rowspan="3">FEL 1 Form Status</th>
+            <th rowspan="3">FEL 2 Form Status</th>
+            <th rowspan="3">FEL 3 Form Status</th>
+            <th rowspan="3">BC Form Status</th>
+
         </tr>
         {{-- column header row 2 start here--}}
         <tr>
@@ -193,6 +199,21 @@
                         @endforeach
                     </ul>
 
+                </td>
+                <td>
+                    {{$p->assessment_status}}
+                </td>
+                <td>
+                    {{$p->fel1_status}}
+                </td>
+                <td>
+                    {{$p->fel2_status}}
+                </td>
+                <td>
+                    {{$p->fel3_status}}
+                </td>
+                <td>
+                    {{$p->bc_status}}
                 </td>
             </tr>
         @endforeach
