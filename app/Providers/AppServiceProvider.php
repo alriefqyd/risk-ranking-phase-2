85e86614f69e7fe10cc8527607984d6864bce01f
Al Riefqy <ekydesmito@gmail.com>
2024-12-09 18:48:11 +0800

    update userservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
-use App\service\UserService;
+use App\Service\UserService;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Gate;
