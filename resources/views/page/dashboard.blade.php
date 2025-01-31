913c0870a099a8a7df2a4686e38f9f53ce875f0e
alriefqy <ekydesmito@gmail.com>
2025-01-31 07:35:14 +0000

    update dashboard

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -6,36 +6,31 @@
 {{--                <li>{{$notification->data['note']}}</li>--}}
 {{--            @endforeach--}}
         </div>
-        <div class="col-sm-12 col-xl-12">
-            <div class="card card-absolute">
-                <div class="card-header bg-primary">
-                    <h5 class="text-white">Welcome</h5>
-                </div>
-                <div class="card-body">
-                    <p style="font-size: 15px" class="mt-4">Risk Ranking Capital Investment and R&D Budget Cycle {{$years}}</p>
+
+        <div class="row mt-2" >
+            <div class="col-sm-8 col-xl-8">
+                <div class="card card-absolute">
+                    <div class="card-header bg-primary">
+                        <h5 class="text-white">Welcome</h5>
+                    </div>
+                    <div class="card-body">
+                        <p style="font-size: 15px" class="mt-4">Risk Ranking Capital Investment and R&D Budget Cycle {{$years}}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="row mt-2" >
-            <div class="col-sm-6 col-xl-4 col-lg-6">
-                <div class="card rounded-3 bg-primary b-primary">
-                    <div class="card-body text-white" style="height: 136px">
-                        <div class="row">
-                            <div class="col-md-7">
-                                <i data-feather="activity" class="ml-2" style="font-size:25px"></i>
-                            </div>
-                            <div class="col-md-5">
-                                <div class="text-end">
-                                    <h5 class="float-right mb-1" style="font-size: 17px">Project</h5>
-                                </div>
-                                <div class="col mb-3 text-end">
-                                    <b class="float-right mb-2 text-center" style="font-size: 17px">{{$projectCount}}</b>
-                                </div>
+            <div class="col-sm-6 col-xl-4 col-lg-6 mt-4">
+                <div class="card o-hidden border-0">
+                    <div class="bg-primary b-r-4 card-body">
+                        <div class="media static-top-widget">
+                            <div class="align-self-center text-center"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-database"><ellipse cx="12" cy="5" rx="9" ry="3"></ellipse><path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path><path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path></svg></div>
+                            <div class="media-body"><span class="m-0">Num of BC</span>
+                                <h4 class="mb-0 counter">{{$projectCount}}</h4><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-database icon-bg"><ellipse cx="12" cy="5" rx="9" ry="3"></ellipse><path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path><path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path></svg>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
 {{--            <div class="col-sm-6 col-xl-4 col-lg-6">--}}
 {{--                <div class="card rounded-3 bg-primary b-primary">--}}
 {{--                    <div class="card-body text-white">--}}
@@ -207,7 +202,7 @@
         </div>--}}
         <div class="row">
             @if($isAdmin)
-            <div class="col-lg-5">
+            <div class="col-lg-6">
                 <div class="card">
                     <div class="card-header pb-0">
                         <h5>Project Submission</h5>
@@ -232,7 +227,7 @@
 {{--                    </div>--}}
 {{--                </div>--}}
 {{--            </div>--}}
-            <div class="col-lg-5 col-sm-6 box-col-6">
+            <div class="col-lg-6 col-sm-6 box-col-6">
                 <div class="card">
                     <div class="card-header pb-0">
                         <h5>Investment Strategy</h5>
