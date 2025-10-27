bf4e1935ef915a8b4f9461a52ee996f1623761fc
alriefqy <ekydesmito@gmail.com>
2025-10-27 14:19:36 +0800

    update migration soft delete project

--- database/migrations/2022_02_14_051629_add_deleted_at_projects.php
+++ database/migrations/2022_02_14_051629_add_deleted_at_projects.php
@@ -13,7 +13,7 @@ class AddDeletedAtProjects extends Migration
      */
     public function up()
     {
-        Schema::table('Projects', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -25,7 +25,7 @@ class AddDeletedAtProjects extends Migration
      */
     public function down()
     {
-        Schema::table('Projects', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             //
         });
     }
